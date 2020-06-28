46de310a051f0e19f81624d90d48032e76e65028
Jeffrey Hooper <jeffreyrepooh@gmail.com>
2020-06-27 22:05:48 -0700

    Add files via upload

--- Poker/card.h
+++ Poker/card.h
@@ -8,4 +8,26 @@ class Card {
  private:
   int value;
   std::string suit;
+};
+
+Card::Card(int value, const std::string& suit) : value(value), suit(std::move(suit)) {}
+
+Card::Card() : Card(-1, "") {}
+
+// form of LHS < RHS
+// ex      5 "Diamonds" < 8 "Hearts"
+bool operator<(const card& rhs) {
+  // note ace has value 1
+  bool lhs_is_ace = this->value == 1;
+  bool rhs_is_ace = rhs.value == 1;
+
+  // ex. 1 < 7
+  if (lhs_is_ace && !rhs_is_ace) {
+    return false;
+  }
+  // ex. 5 < 1
+  if (rhs_is_ace && !lhs_is_ace) {
+    return true;
+  }
+  return this->value < rhs.value;
 }
