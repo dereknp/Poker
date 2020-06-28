a7d1237b43b6a572322875098ddba0eed054d9b6
dereknp <58238099+dereknp@users.noreply.github.com>
2020-06-27 22:12:58 -0700

    Add files via upload

--- Poker/card.h
+++ Poker/card.h
@@ -1,3 +1,4 @@
+#pragma once
 #include <iostream>
 
 class Card {
@@ -17,17 +18,17 @@ Card::Card() : Card(-1, "") {}
 // form of LHS < RHS
 // ex      5 "Diamonds" < 8 "Hearts"
 bool operator<(const card& rhs) {
-  // note ace has value 1
-  bool lhs_is_ace = this->value == 1;
-  bool rhs_is_ace = rhs.value == 1;
+	// note ace has value 1
+	bool lhs_is_ace = this->value == 1;
+	bool rhs_is_ace = rhs.value == 1;
 
-  // ex. 1 < 7
-  if (lhs_is_ace && !rhs_is_ace) {
-    return false;
-  }
-  // ex. 5 < 1
-  if (rhs_is_ace && !lhs_is_ace) {
-    return true;
-  }
-  return this->value < rhs.value;
+	// ex. 1 < 7
+	if (lhs_is_ace && !rhs_is_ace) {
+		return false;
+	}
+	// ex. 5 < 1
+	if (rhs_is_ace && !lhs_is_ace) {
+		return true;
+	}
+	return this->value < rhs.value;
 }
--- Poker/game.h
+++ Poker/game.h
@@ -1,6 +1,8 @@
 #pragma once
+#include <iostream>
 #include "deck.h"
 #include "player.h"
+#include "game.h"
 
 class game
 {
@@ -18,3 +20,49 @@ public:
   void deal_river();
   void winner();
 };
+
+game::game()
+{
+  pot_value = 0;
+}
+
+void game::show_game_info()
+{
+  std::cout << "Chips in pot: " << pot_value << std::endl;
+  for (int i = 0; i < 5; i++)
+  {
+    std::cout << shared_cards[i].suit << shared_cards[i].value << std::endl;
+  }
+}
+
+void game::deal()
+{
+  d.shuffle_deck();
+  x.get_hand(d.get_card(), d.get_card());
+  x.show_info();
+  pot_value += x.bet(pot_value);
+}
+
+void game::deal_flop()
+{
+  for (int i = 0; i < 3; i++)
+  {
+    shared_cards[i] = d.get_card();
+  }
+  show_game_info();
+  pot_value += x.bet(pot_value);
+}
+
+void game::deal_turn()
+{
+  shared_cards[3] = d.get_card();
+  pot_value += x.bet(pot_value);
+  show_game_info();
+}
+
+void game::deal_river()
+{
+  shared_cards[4] = d.get_card();
+  pot_value += x.bet(pot_value);
+  show_game_info();
+}
